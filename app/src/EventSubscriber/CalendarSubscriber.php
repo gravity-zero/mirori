cc00749f62e5552b0847779f60a0b5ec8c4e6f90
ismail94370 <isma_abbes@outlook.fr>
2022-06-02 14:41:36 +0200

    Création du calendrier avec ajout

---
+++ app/src/EventSubscriber/CalendarSubscriber.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use App\Repository\BookingRepository;
+use CalendarBundle\CalendarEvents;
+use CalendarBundle\Entity\Event;
+use CalendarBundle\Event\CalendarEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class CalendarSubscriber implements EventSubscriberInterface
+{
+    private $bookingRepository;
+    private $router;
+
+    public function __construct(
+        BookingRepository $bookingRepository,
+        UrlGeneratorInterface $router
+    ) {
+        $this->bookingRepository = $bookingRepository;
+        $this->router = $router;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            CalendarEvents::SET_DATA => 'onCalendarSetData',
+        ];
+    }
+
+    public function onCalendarSetData(CalendarEvent $calendar)
+    {
+        $start = $calendar->getStart();
+        $end = $calendar->getEnd();
+        $filters = $calendar->getFilters();
+
+        $bookings = $this->bookingRepository
+            ->createQueryBuilder('booking')
+            ->where('booking.beginAt BETWEEN :start and :end OR booking.endAt BETWEEN :start and :end')
+            ->setParameter('start', $start->format('Y-m-d H:i:s'))
+            ->setParameter('end', $end->format('Y-m-d H:i:s'))
+            ->getQuery()
+            ->getResult()
+        ;
+
+        foreach ($bookings as $booking) {
+            // je crée un event avec les données pour remplir le calendrier 
+            $bookingEvent = new Event(
+                $booking->getTitle(),
+                $booking->getBeginAt(),
+                $booking->getEndAt() 
+            );
+
+            $bookingEvent->setOptions([
+                'backgroundColor' => 'red',
+                'borderColor' => 'red',
+            ]);
+            $bookingEvent->addOption(
+                'url',
+                $this->router->generate('app_booking_show', [
+                    'id' => $booking->getId(),
+                ])
+            );
+
+            $calendar->addEvent($bookingEvent);
+        }
+    }
+}
