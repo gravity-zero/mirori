69d007cc546f73bb841caf2021abfeddcb2047db
Hanna <hanna.achab@hetic.net>
2022-07-05 12:25:23 +0200

    feat(header): Add header component

--- front-app/src/pages/exhibitors/exhibitors.tsx
+++ front-app/src/pages/exhibitors/exhibitors.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import SearchBar from '../../components/searchBar/searchBar';
+import Header from '../../components/header/header'
 import { ExhibitorsContainer } from './exhibitors-style'
 
 const Exhibitors: React.FC = () => {
 
+  // const date = new Date();
+  // var tab_mois=new Array("Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre");
+
   return (
-    <ExhibitorsContainer>
-      <SearchBar
-        placeholder="Rechercher un exposant"></SearchBar>
-    </ExhibitorsContainer>
+    <div>
+      {/* <Header></Header> */}
+      <Header></Header>
+      <ExhibitorsContainer>
+        <SearchBar
+          placeholder="Rechercher un exposant"></SearchBar>
+      </ExhibitorsContainer>
+    </div>
+
   )
 }
 
