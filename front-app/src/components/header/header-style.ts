0a2d5df3d88e3ebc769fa2863986cb5eea58eb74
Hanna <hanna.achab@hetic.net>
2022-07-06 16:34:38 +0200

    feat: Add QR Code

---
+++ front-app/src/components/header/header-style.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const HeaderContainer = styled.div`
+  width: 100vw;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const DateContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-size: 48px;
+  align-items: center;
+  margin: 78px 34px;
+`;
+
+export const HourContainer = styled.div`
+  font-size: 48px;
+  margin: 78px 34px;
+`;
--- front-app/src/pages/exhibitors/exhibitors.tsx
+++ front-app/src/pages/exhibitors/exhibitors.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import SearchBar from '../../components/searchBar/searchBar';
+import Subscription from '../admin/subscription';
 import { ExhibitorsContainer } from './exhibitors-style'
 
-const Exhibitors: React.FC = () => {
+const Exhibitors: React.FC = ({
+
+}) => {
 
   return (
-    <ExhibitorsContainer>
-      <SearchBar
-        placeholder="Rechercher un exposant"></SearchBar>
-    </ExhibitorsContainer>
+    <div>
+      <ExhibitorsContainer>
+        <SearchBar
+          placeholder="Rechercher un exposant"></SearchBar>
+      </ExhibitorsContainer>
+      <Subscription></Subscription>
+    </div>
+
   )
 }
 
