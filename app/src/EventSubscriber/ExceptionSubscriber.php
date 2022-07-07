b0cd55f54bf732ab8477230ce47d53c2dfb6913b
ismail94370 <isma_abbes@outlook.fr>
2022-07-07 17:30:25 +0200

    correction fichier subscriber

--- app/src/EventSubscriber/ExceptionSubscriber.php
+++ app/src/EventSubscriber/ExceptionSubscriber.php
@@ -5,49 +5,10 @@ namespace App\EventSubscriber;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
-use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Psr\Container\ContainerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
-
-
-
-
 
 class ExceptionSubscriber implements EventSubscriberInterface
 {
-
-    /**
-     * @var ContainerInterface
-     */
-    protected $container;
-
-    /**
-     * @required
-     */
-    public function setContainer(ContainerInterface $container): ?ContainerInterface
-    {
-        $previous = $this->container;
-        $this->container = $container;
-
-        return $previous;
-    }
-
-    protected function json($data, int $status = 200, array $headers = [], array $context = []): JsonResponse
-    {
-        if ($this->container->has('serializer')) {
-            $json = $this->container->get('serializer')->serialize($data, 'json', array_merge([
-                'json_encode_options' => JsonResponse::DEFAULT_ENCODING_OPTIONS,
-            ], $context));
-
-            return new JsonResponse($json, $status, $headers, true);
-        }
-
-        return new JsonResponse($data, $status, $headers);
-    }
-
-
     public static function getSubscribedEvents()
     {
         // return the subscribed events, their methods and priorities
@@ -68,14 +29,11 @@ class ExceptionSubscriber implements EventSubscriberInterface
             $exception->getCode()
         );
 
-        // Customize your response object to-   display the exception details
         $response = new JsonResponse(
             ['message' => $message]
         );
 
-        // sends the modified response object to the event
         $event->setResponse($response);
-
     }
 
     public function logException(ExceptionEvent $event)
