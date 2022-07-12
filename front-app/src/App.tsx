0c8df328d2c726a4b45ac62c2c973f1ec979b7b2
Hanna <hanna.achab@hetic.net>
2022-07-12 16:54:47 +0200

    feat: Add routes and pages

--- front-app/src/App.tsx
+++ front-app/src/App.tsx
@@ -8,6 +8,8 @@ import Events from './pages/visitors/events/events';
 import Schedule from './pages/visitors/schedule/schedule';
 import ValueContextProvider from './Context/useIsMobile';
 import Header from './components/header/header';
+import FacialRecognitionFailed from './pages/visitors/facialRecognitionFailed';
+import StandbyMode from './pages/visitors/standbyMode';
 
 
 
@@ -19,11 +21,12 @@ function App() {
         <Header />
         <Routes>
           <Route path='visitors/facialRecognitionLoading' element={<Loading />} ></Route>
-          <Route path='visitors/facialRecognitionFailed' element={<Loading />} ></Route>
+          <Route path='visitors/facialRecognitionFailed' element={<FacialRecognitionFailed />} ></Route>
           <Route path='/' element={<Home />} ></Route>
           <Route path='visitors/exhibitors' element={<Exhibitors />} ></Route>
           <Route path='visitors/events' element={<Events />} ></Route>
           <Route path='visitors/schedule' element={<Schedule />} ></Route>
+          <Route path='visitors/standbyMode' element={<StandbyMode />} ></Route>
         </Routes>
       </ValueContextProvider>
     </div>
---
+++ front-app/src/pages/visitors/authFacialRecognition.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Loading: React.FC = () => {
+  return (
+    <h1>Route jwt Auth reconnaissance facial </h1>
+  )
+}
+
+export default Loading;
