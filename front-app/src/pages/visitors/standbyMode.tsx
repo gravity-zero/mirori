44aba02492d887e15f866709aada9f2d7d9cbddb
Hanna <hanna.achab@hetic.net>
2022-09-12 23:18:08 +0200

    fix: remove unused code and run build

--- front-app/src/pages/visitors/standbyMode.tsx
+++ front-app/src/pages/visitors/standbyMode.tsx
@@ -5,12 +5,10 @@ const StandbyMode: React.FC = () => {
 
   useEffect(() => {
     localStorage.clear();
-
-  })
+  }, [])
 
   return (
     <Container>
-      <script>window.location.reload()</script>
       <img src="/assets/gif/giphy.gif" alt="" />
     </Container>
   )
